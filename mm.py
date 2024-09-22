93697b78ede5460b3bbf4c96175a4d381efe28e3
Md. Moshiur Rahman <113328325+Moshiur-rahman74@users.noreply.github.com>
2024-09-22 15:41:09 +0600

    Add files via upload

---
+++ mm.py
@@ -0,0 +1,21 @@
+a="hello"
+b="b2"
+c="333      "
+d=a+""+b+""+c
+print(d)
+print(len(d))
+print(d[:-3])
+print(d[-3:])
+print(d[::-1])
+print(d[-1::])
+print(d.upper())
+print(d.lower())
+print(d.strip())
+print(d.split())
+v=lambda x,y,z:x*y*z
+print(v(5, 6, 2))
+q=int(input("Enter"))
+w=int(input("Enter"))
+f=lambda q,w: q+w
+print(f(q,w))
+
